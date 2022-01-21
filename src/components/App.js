ff7470a54beda0c2c52a1767eef515475a8e9088
Norma <>
2022-01-21 17:04:48 -0300

    base de datos barajada en app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,76 @@
-//
-// Para incluir los diferentes sets de cartas podemos _importar_ el archivo
-// JavasSript que contenga el `export` correspondiente...
-//
-// import pokemon from '../data/pokemon/pokemon.js';
-// console.log(pokemon);
-//
-// O alternativamente podríamos cargar el JSON de forma asíncrona usando
-// `fetch` en el momento que consideremos necesario.
-//
-// fetch('./data/pokemon/pokemon.json')
-//   .then(resp => resp.json())
-//   .then(console.log)
-//   .catch(console.error);
-//
+
+import Pokemon from '../data/pokemon/pokemon.js';
+
+//export default Pokemon;
+//console.log(Pokemon);
 
 const App = () => {
-  const el = document.createElement('div');
 
-  el.className = 'App';
-  el.textContent = 'Hola mundo!';
+  const cardsItems = Pokemon.items;
+  const cardsItemsTwo = Pokemon.items;
+  const cardsTotal = cardsItems.concat(cardsItemsTwo);
 
-  return el;
-};
+  //Seleccionamos el board 
+  //const board = document.querySelector(".board");
+
+  let i = cardsTotal.length, j, temp;
+  while (--i > 0) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = cardsTotal[j];
+    cardsTotal[j] = cardsTotal[i];
+    cardsTotal[i] = temp;
+    //return cardsTotal;
+  }
+
+  //console.log(cardsTotal);
+
+
+  //funcion que le de un movimiento aleatorio a las cartas
+  //
+
+
+
+  // con esto crearemos la estructura
+  //const App = () => {
+  // contenedor padre del juego
+  const pokemonContainer = document.createElement('div');
+  pokemonContainer.className = 'pokemonContainer';
+
+  //contenedor del grid de cartas
+  const cardsGrid = document.createElement('div');
+  cardsGrid.id = 'cardsGrid';
+  cardsGrid.className = 'cardsGrid';
+  pokemonContainer.appendChild(cardsGrid);
+  //console.log(cardsGrid);
+
+  //mostrar las cartas
+  const boardCards = () => {
+    for (let i = 0; i < cardsTotal.length; i++) {
+      //console.log(cardsTotal[i].id)
+      const boardCards = cardsTotal.innerHTML('cardsGrid')
+    
+    
+  }
+}
+
+
+
+  //el.className = 'App';
+  // el.textContent = 'Hola mundo!';
+
+  //return el;
+}
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
