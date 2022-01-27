75e55aff44856b22e652ecd76cd081efa0528bfe
Norma <>
2022-01-27 11:52:00 -0300

    cambios css, se pueden voltear las cartas, js empezar click y match

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 import shuffle from '../shuffle.js';
 
 const App = () => {
+
+
+
   // contenedor padre del juego
   const pokemonContainer = document.createElement('div');
   pokemonContainer.className = 'pokemonContainer';
-  // contenedor hijos de pokemonContainer, donde van a estar las cartas
-  //const gridCard = document.createElement('div');
- // gridCard.className = "gridCard"
-  //gridCard.appendChild()
-  // recorro todas las cartas
+
   for (let i = 0; i < shuffle.length; i++) {
     //console.log(cardsTotal[i].id)
     const card = document.createElement('div');
@@ -17,26 +16,57 @@ const App = () => {
     card.className = "card";
     front.className = "front";
     back.className = "back";
-    
+    pokemonContainer.appendChild(card);
+    card.appendChild(back);
+    card.appendChild(front);
+
+
     const imgFront = document.createElement('img');
     front.appendChild(imgFront);
     imgFront.className = "imgFront"
+    front.setAttribute('src', shuffle[i].image);//insertar imagen front  
 
-    //insertar imagen back 
-    front.setAttribute('src', './img/carta-1.png');
-    //creo imgFront hijo de front insertar imagen 
+      //creo imgFront hijo de front insertar imagen 
     const imgBack = document.createElement('img');
+    back.setAttribute('src', './img/carta-1.png')  //insertar imagen back 
     back.appendChild(imgBack);
-    imgBack.className="imgFront"
+    imgBack.className = "imgFront"
     imgBack.id = shuffle[i].id;
-    //insertar imagen front  
-    back.setAttribute('src', shuffle[i].image);
-    pokemonContainer.appendChild(card);
-    card.appendChild(front);
-    card.appendChild(back);
-  }
-  return pokemonContainer;
-}
+
+    // FUNCION FLECHA
+    //init: function () {
+    // console.log('Los siguientes pilotos van a comenzar la carrera:', this.drivers);
+    //setTimeout(() => console.log('El ganador es', randomWinner(this.drivers)), 1000);
+    //} 
+    // tenemos 2 variables, clickCard y matchCard que son igual a un array
+
+    var clickCard = []
+    //var matchCard = [];
+    // a card le agrego un evento para clickear las carta 
+    card.addEventListener('click', function () {
+      //si  logitud de la cartas clickedas es menor a 2
+      if (clickCard.length < 2) {
+        back.setAttribute('src', './img/carta-1.png')  //insertar imagen back 
+        //click car agrega con push a todas las cartas barajadas
+        //clickCard.push(shuffle[i]);
+        }
+        });
+       //console.log(clickCard);
+
+
+
+
+
+      }
+
+      return pokemonContainer;
+
+
+
+
+    }
+
+
 export default App;
 
 
